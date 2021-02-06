08340438069f8568903580247b540f2cf622e1db
AlidaSchefers <alidadschefers@gmail.com>
2021-02-05 22:36:47 -0500

    initial commit

---
+++ backend/middleware/calctimezone.js
@@ -0,0 +1,7 @@
+module.exports = (offset) => {
+    const d = new Date();
+    const utc = d.getTime() + (d.getTimezoneOffset() * 60000); //in msec
+    const nd = new Date(utc + (3600000*offset));
+
+    return nd.toLocaleString() //e.g. 2/5/2021, 12:04:02 PM for Eastern Standard Time (GMT-5)
+}
