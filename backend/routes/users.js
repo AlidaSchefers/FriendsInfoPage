4c2d6be5cf142bb783c0af3b30959a27150d70a6
AlidaSchefers <alidadschefers@gmail.com>
2021-02-09 22:50:04 -0500

    created working createFriend and deleteFriend requests

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -44,22 +44,14 @@ router.post('/login', async (req, res, next) => { //should review when to use ne
 
 router.post('/createFriend', async (req, res, next) => {
     try {
-        // const UserDB = await User.findOne({email: req.body.email}) //restrict login to email, not username
-        // const friendAddition = {name: req.name, location: req.location, timeOffset: 8}
-        // UserDB.friends = friendAddition
-        UserDB.friends = {name: req.name, location: req.location, timeOffset: 8}
-        await User.updateOne({email: req.body.email}, 
-            {$set: {
-                    friends: {name: req.name, location: req.location, timeOffset: 8}
-                }
-            })
-        const UserDB = await User.findOne({email: req.body.email}) //restrict login to email, not username
+        let UserDB = await User.findOne({email: req.body.email}) //restrict login to email, not username
+        //adds a new friend
+        UserDB.friends = [...UserDB.friends, {name: req.body.name, location: req.body.location, timeOffset: 8}] //now we need to do the timezoneAPI before friend assignment!
+        await UserDB.save()
         res.send(UserDB)
-        
-        // await UserDB.save()
         // .then(savedUserDB => {
         //     console.log(savedUserDB === UserDB)
-        //     res.send(UserDB)
+        //     res.send(savedUserDB)
         //     // console.log(savedUserDB)
         // })
         // .catch(error => {
@@ -70,30 +62,16 @@ router.post('/createFriend', async (req, res, next) => {
     }
 })
 
-
-// router.put('/', verify, async (req,res,next) => { //in this, the user's password is sent to the router and then hashed then updated in DB.
-//     //put more try-catches or if-else so that the errors are more specific
-//     try {
-//         const DBUser = await User.findOne({_id: req.user._id})
-//         const currentDBHashedPassword = await DBUser.password
-//         if(await bcrypt.compare(req.body.password, currentDBHashedPassword)) { //if the current password is correct
-//             const newHashedPassword = await bcrypt.hash(req.body.newpassword, 10) //need newpassword property in the req body
-//             const updatedUser = await User.findOneAndUpdate({_id: req.body._id}, {password: newHashedPassword}, {useFindAndModify: false, new: true}) //BUG
-//                 //this also returns the document. with the option ({new: true}), it returns the doc after the update
-//             if(await bcrypt.compare(req.body.newpassword, updatedUser.password)) {
-//                 res.json({message: "Password successfully updated."})
-//             }else{
-//                 res.json({message: "The password has not been updated."})
-//             }
-//             next()
-//         }
-//     } catch (error) {
-//         res.json({message: error.message || error})
-//     }
-// })
-
-
-router.post('/deleteFriend')
+router.post('/deleteFriend', async (req, res, next) => {
+    try {
+        let UserDB = await User.findOne({email: req.body.email})
+        UserDB.friends = UserDB.friends.filter(friend => friend.name != req.body.name)
+        await UserDB.save()
+        res.send(UserDB)
+    } catch (error) {
+        res.status(500).json({message: error.message || error})
+    }
+})
 
 //send _id of user, then modify friends' info.
 
