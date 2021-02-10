de8472d744f088bb8c517f1a4c36ccfb6f33c23b
AlidaSchefers <alidadschefers@gmail.com>
2021-02-10 18:18:12 -0500

    completed verification function

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,5 +1,13 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = function(req, res, next) {
-    const token = req.headers{'auth-token'}
+    const token = req.headers['auth-token']
+    if(!token) return res.status(401).send('Access Denied')
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        req.user = verified //verified returns { _id: '6023e9d528f50c4f24f4e728', iat: 1612966385 }
+        next()
+    } catch (error) {
+        res.status(400).send('Invalid Token')
+    }
 }
