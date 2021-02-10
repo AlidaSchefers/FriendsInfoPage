fab8113126cf702b7ed672084c4ffeb54ce00ca8
AlidaSchefers <alidadschefers@gmail.com>
2021-02-10 18:46:49 -0500

    changed comments

--- backend/middleware/calctimezone.js
+++ backend/middleware/calctimezone.js
@@ -1,7 +1,7 @@
-module.exports = (offset) => {
+module.exports = (offset) => { //offset will be in hours (e.g. UTC-5)
     const d = new Date();
     const utc = d.getTime() + (d.getTimezoneOffset() * 60000); //in msec
-    const nd = new Date(utc + (3600000*offset));
+    const nd = new Date(utc + (3600000*offset)); //in msec still.
 
     return nd.toLocaleString() //e.g. 2/5/2021, 12:04:02 PM for Eastern Standard Time (GMT-5)
 }
