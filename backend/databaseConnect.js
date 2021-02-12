72cb2357376314baff3310a39e2e95b1955ca1e4
AlidaSchefers <alidadschefers@gmail.com>
2021-02-11 22:41:18 -0500

    added parameter property

--- backend/databaseConnect.js
+++ backend/databaseConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = () => {
-    mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     
     mongoose.connection.on('connected', () => {
         console.log('connecting to database')
