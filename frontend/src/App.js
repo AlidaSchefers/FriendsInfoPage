6a0caf45e3c97843641f625774f5691aef828ee3
AlidaSchefers <alidadschefers@gmail.com>
2021-02-12 22:57:56 -0500

    included frontpage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React from 'react'
+import Frontpage from './components/Frontpage'
 
 function App() {
   return (
-    
+    <Frontpage />
   );
 }
 
