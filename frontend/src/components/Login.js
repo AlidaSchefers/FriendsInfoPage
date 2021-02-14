07e7ef917e6e0d3d8057b39f0873d4973f9a4a37
AlidaSchefers <alidadschefers@gmail.com>
2021-02-14 13:27:41 -0500

    fixed the start script

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import axios from 'axios'
+import Form from './Form'
 
+const inputs = [
+    {name: 'email', type: 'text', placeholder: 'Enter Your Email'},
+    {name: 'password', type:'password', placeholder: 'Enter Your Password'}
+]
+
+const submitMsg = "login"
+
+const submitFunc = (formData, resetForm) => {
+    axios.post('http://localhost:4000/users/login', formData)
+    .then(res => {
+        alert("Logged in.")
+        console.log(`token: ${res.data}`)
+        localStorage.setItem('token', res.data)
+
+        resetForm()
+    })
+    .catch(err => {
+        console.log(err)
+        alert('Invalid credentials.')
+    })
+}
 
 export default function Login() {
     return(
-        <div>
+        <div id="login">
             This is the login page
+        <Form inputs={inputs} submitMsg={submitMsg} submitFunc={submitFunc} />
         </div>
     )
 }
