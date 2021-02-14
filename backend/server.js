3e9737183d08f1f066b655a04c94bcf40cfec45c
AlidaSchefers <alidadschefers@gmail.com>
2021-02-14 13:33:00 -0500

    allowed my frontend to communicate with my backend with CORs package

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ const server = express()
 const usersRouter = require('./routes/users')
 require('dotenv').config()
 const databaseConnect = require('./databaseConnect')
+const CORs = require('cors')
 
 databaseConnect()
+server.use(CORs({
+    origin: ['http://localhost:3000'] //allows only my frontend server to connect with backend server
+}))
 
 // const friendsInfoAPIRouter = require('./routes/friendsInfoAPI')
 // server.use('/getFriendsInfoTimeZone', friendsInfoAPIRouter)
@@ -17,9 +21,10 @@ server.use(express.json()) //expect that every request potentially have JSON. Al
 
 server.use('/users', usersRouter)
 
-
+const port = 4000
 //what's the point of listening?
-server.listen(4000, () => { //use 4000 for backend and 3000 for frontend. Why?
-    console.log('Listening on port 4000')
+server.listen(port, () => { //use 4000 for backend and 3000 for frontend. Why?
+    console.log(`Listening on port ${port}`)
+    console.log(`ORs-enabled webserver running on port ${port}`)
     // console.log(new Date()) //why does this not show my time? but it does in middleware/calctimezone ?
 }) 
