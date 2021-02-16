ccdf7cde79ced0971dfa565ff2a98e41936a801c
AlidaSchefers <alidadschefers@gmail.com>
2021-02-15 22:12:55 -0500

    simplified code for testing

--- frontend/src/components/DisplayFriends.js
+++ frontend/src/components/DisplayFriends.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 export default function DisplayFriends() {
+    // const renderLogoutButton = () => { //when press the button, console.log does not show. only until refresh or change page compnents (like do to welcome page)
+    //     console.log(`token Navigation2: ${localStorage.getItem('token')}`)
+    //     return (
+    //             <button
+    //                     onClick={() => localStorage.setItem('token', null)}
+    //             >Logout</button>
+    //         )
+    // }
+    
+    
     return(
         <div>
-        This will eventually display friends info.
+        FRIENDS DISPLAY
+        {/* <button
+                        onClick={() => {
+                            localStorage.setItem('token', null)
+                            console.log(localStorage.getItem('token'))
+                        }}
+                >Logout</button> */}
         </div>
     )
 }
