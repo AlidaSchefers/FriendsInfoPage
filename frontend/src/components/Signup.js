a756e3fbee77743d75642eea48f73451464a03ad
AlidaSchefers <alidadschefers@gmail.com>
2021-02-17 19:02:53 -0500

    added comments

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -30,31 +30,13 @@ const submitFunc = (formData, resetForm) => {
         //after user is successfully created, clear the input fields! 
         resetForm() 
     })
-    .catch(error => {
-        console.log(error);
-        // console.log("oh no! It didn't work.")
-        const errorsArr = error.response.data.errors
-        let alertStr = ""
-        for(let i = 0; i < errorsArr.length; i++) {
-            alertStr += `${errorsArr[i].msg}\n`
-            //could be better/simpler
-        }
-        alert(alertStr)
-    })
-
-    // axios.post('localhost:4000/users/signup', formData)
-    // .then(response => {
-    //     console.log(response);
-    // })
-    // .catch(error => {
-    //     console.log(error);
-    // })
+    .catch() //we are not checking for invalid emails or passwords, no just leave it like this.
 }
 
 
 export default function Signup() {
     return(
-        <div>
+        <div id="signup">
             This is the signup page
             <Form inputs={inputs} submitMsg={submitMsg} submitFunc={submitFunc}/>
         </div>
