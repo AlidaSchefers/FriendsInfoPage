d822fab2796ce953b5635c0331dce6ff14232f5a
AlidaSchefers <alidadschefers@gmail.com>
2021-02-20 22:54:28 -0500

    added AddFriend component

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,3 +1,9 @@
 //this will be the component where users add/delete friends
-
 //this will require using the Form component, requests to the database, and a button to return to the friends' display page
+import AddFriend from'./addFriend'
+
+export default function Settings() {
+    return(
+        <AddFriend />
+    )
+}
