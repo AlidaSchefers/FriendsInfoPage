b5a1d281e2d8188a1c4261f69040c0ddbf90847e
AlidaSchefers <alidadschefers@gmail.com>
2021-02-22 22:21:20 -0500

    added token to post request

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -2,6 +2,16 @@ const jwt = require('jsonwebtoken')
 
 module.exports = function(req, res, next) {
     const token = req.headers['auth-token']
+    // const token = req.headers['auth-token'] ? req.headers['auth-token'] : localStorage.getItem('token')
+    // const token = req.body['auth-token']
+    console.log(`header auth-token:`)
+    console.log(req.headers['auth-token'])
+    console.log(`!token:`)
+    console.log(!token)
+    // console.log(`local storage token:`)
+    // console.log(localStorage.getItem('token'))
+    // const token = localStorage.getItem('token')
+    // const token = req.body['auth-token']
     if(!token) return res.status(401).send('Access Denied')
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET_KEY)
