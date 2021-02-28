60ea7de66b716c43c99c88ce8acae07d144292d9
AlidaSchefers <alidadschefers@gmail.com>
2021-02-27 22:45:59 -0500

    pulls correct section from the response

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -4,38 +4,45 @@ import axios from 'axios'
 //get request for friends names and locations.
 //return JSX component for table of each friend with a 'delete friend' button for each one. 
 //this will list the friends the user currently has
-const token = localStorage.getItem('token')
+// const token = localStorage.getItem('token')
 
-const responseData = axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
-console.log('responseData from getFriendsList route') //this is the array of objects
-console.log(responseData) //this is the array of objects
+// const responseData = axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
+// console.log('responseData from getFriendsList route') //this is the array of objects
+// console.log(responseData) //this is the array of objects
+// console.log(responseData) //this is the array of objects
 
 // const {data} = axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
 // console.log('data from getFriendsList route') //this is the array of objects
 // console.log(data)
 
-const rowsOfFriends = (res) => {
-    let rows = ""
-    for(let i = 0; i < res.length; i++) {
-        rows += `<tr> <td>${res[i].name}</td> <td>${res[i].location}</td> </tr>`
-    }
-    return rows
-}
+// const rowsOfFriends = (res) => {
+//     let rows = ""
+//     for(let i = 0; i < res.length; i++) {
+//         rows += `<tr> <td>${res[i].name}</td> <td>${res[i].location}</td> </tr>`
+//     }
+//     return rows
+// }
 
 export default function FriendsList() {
-    // const token = localStorage.getItem('token')
-    // const token = localStorage.getItem('token')
-    // axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
-    // .then(res => { //res is an array of objects
-    //     let rowsOfFriends = ""
-    //     for(let i = 0; i < res.length; i++) {
-    //         rowsOfFriends += `<tr> <td>${res[i].name}</td> <td>${res[i].location}</td> </tr>`
-    //     }
-    // })
-    // .catch(err => {
-    //     console.log(err)
-    //     alert('Invalid credentials.')
-    // })
+    let rowsOfFriends = ""
+    const token = localStorage.getItem('token')
+
+    // const {data} = await axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
+    // console.log('data from getFriendsList route') //this is the array of objects
+    // console.log(data)
+
+    axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
+    .then(res => { //res is an array of objects
+        // console.log("res:")
+        // console.log(res.data)
+        for(let i = 0; i < res.data.length; i++) {
+            rowsOfFriends += `<tr><td>${res.data[i].name}</td><td>${res.data[i].location}</td></tr>`
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        alert('Invalid credentials.')
+    })
 
     return (
         <table>
@@ -44,7 +51,8 @@ export default function FriendsList() {
                     <th>Name</th>
                     <th>Location</th>
                 </tr>
-                {rowsOfFriends(responseData)} 
+                {/* <tr> <td>Name1</td> <td>Location1</td> </tr> */}
+                {rowsOfFriends} 
                 {/* {rowsOfFriends(data)}  */}
             </tbody>
         </table>
