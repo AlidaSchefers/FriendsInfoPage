957a2dc8b830a115f7c1129d008e73092b49212f
AlidaSchefers <alidadschefers@gmail.com>
2021-03-02 23:09:15 -0500

    added delete friend button and added more de-bugging code

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 import parse from 'html-react-parser'
+import { values } from '../config/navButtonsConfig'
 
 //get request for friends names and locations.
 //return JSX component for table of each friend with a 'delete friend' button for each one. 
@@ -17,8 +18,9 @@ import parse from 'html-react-parser'
 // console.log(data)
 
 export default function FriendsList() {
-    let rowsOfFriends
-    // let firstFriend
+    // let rowsOfFriends
+    let firstFriend
+    let stringOfFriendDisplay
     // let firstFriendString
     const token = localStorage.getItem('token')
 
@@ -49,22 +51,61 @@ export default function FriendsList() {
     //     alert('Invalid credentials.')
     // })
 
-    const [listOfFriendsState, setListOfFriendsState] = useState({})
+    // const [listOfFriendsState, setListOfFriendsState] = useState([])
 
     const getFriendsWithAxios = async () => {
+        // const {data} = await axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
         const response = await axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
         // const jsonData = await response.json()
         // setListOfFriendsState(response.data)
-        console.log("response.data:")
-        console.log(response.data)
+
+        // firstFriend = response.data
+        // console.log("response.data:")
+        // console.log(response.data)
+        // console.log("firstFriend inside function with request:")
+        console.log('res.data inside request')
+        console.log(response.data) //returns the array of firends objects
+        return response.data
+
     }
-    getFriendsWithAxios()
+    console.log("return of the function:")
+    console.log(getFriendsWithAxios()) //returns a promise
+    // console.log("return of the function EDITED:")
+    // console.log(getFriendsWithAxios().data)
+    
+
+    console.log("firstFriend OUTside function with request:")
+    console.log(firstFriend)
+
+    //firstFriend WORKS! array of objects, with each object being a friend
+
+    // for(let i = 0; i < firstFriend.length; i++) {
+    //     stringOfFriendDisplay += <tr><td>${firstFriend[i].name}</td><td>${firstFriend[i].location}</td></tr>
+    //     console.log("stringOfFriendDisplay inside for loop:")
+    //     console.log(stringOfFriendDisplay)
+    // }
+
+    
 
     // console.log("listOfFriendsState:")
     // console.log(listOfFriendsState)
 
+    console.log("firstFriend outside function:")
+    console.log(firstFriend)
+
+    //make button for deleting a friend with the route /deleteFriend
 
 
+    const renderDeleteButtons = () => {
+        return(
+                    <button
+                        // onClick={}
+                    >Delete friend
+                    </button>
+                )
+    }
+
+    {
     // console.log("rowsOfFriends outside of loop:")
     // console.log(rowsOfFriends)
 
@@ -74,6 +115,7 @@ export default function FriendsList() {
 
     // const name2 = 'Name101'
     // const testFriend = `<tr><td>${name2}</td><td>Paris, France</td></tr>`
+    }
 
     return (
         <table>
@@ -82,7 +124,7 @@ export default function FriendsList() {
                     <th>Name</th>
                     <th>Location</th>
                 </tr>
-                {/* <tr><td>Name1</td><td>Paris, France</td></tr> */}
+                <tr><td>Name1</td><td>Paris, France</td><td>{renderDeleteButtons()}</td></tr>
                 {/* {rowsOfFriends}  */}
                 {/* {parse(testFriend)}  */}
             </tbody>
