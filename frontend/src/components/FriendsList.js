d74ded351e9a81b447af7418b92feb4961276f82
AlidaSchefers <alidadschefers@gmail.com>
2021-03-30 20:29:13 -0400

    successfully displays friends in a table

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,10 +1,12 @@
+// model this off of GetaWeaherPic component!!
+
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import parse from 'html-react-parser'
 import { values } from '../config/navButtonsConfig'
 // import {useFriendsInfoContext} from '../contexts/friendsInfoContext'
-// const token = localStorage.getItem('token')
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRiNmI2ZGI3ZmU2NTIyZGM4NTQ1ZTQiLCJpYXQiOjE2MTU1NTY1OTB9.po2EGAJ-OGJp-jWFN7gU64hsJ0cemAtaYngpNjJpvGE'
+const token = localStorage.getItem('token')
+// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDYyODk0MTJiNmYyNDFlNWQ4ODEzOTMiLCJpYXQiOjE2MTcwNzA0MjJ9.5Ycntu3iYR35J4JFNMqnYKq0Cp69UEU02MogDO0bJZg'
 
 
 //get request for friends names and locations.
@@ -14,58 +16,71 @@ const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRiNmI2ZGI3ZmU2
 
 //-----------------------------
 
-const useAxiosToGetFriendsArr = () => {
+const GiveFriendsArr = () => {
+    console.log("token:")
+    console.log(token)
     
+    const [listOfFriendsState, setListOfFriendsState] = useState([])
     let listOfFriends = '';
-    const [listOfFriendsState, setListOfFriendsState] = useState({})
+
     useEffect(() => {
-        axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
-        .then(({data}) => {
-            // setListOfFriendsState({data})
-            // console.log(data[0].name)
-            listOfFriends += data[0].name
-        })
+        getFriendsArrWAxios()
     }, []); //no [] or [localStorage.getItem('lsFriendsList')]
-    // for(let i = 0; i < listOfFriendsState.length; i++) {
-    //     // listOfFriends += `<tr><td>${listOfFriendsState[i].name}</td><td>${listOfFriendsState[i].location}</td></tr>`
-    console.log("listOfFriendsStateA")    
-    console.log(listOfFriendsState)    
-    // listOfFriends += '<tr><td>'+listOfFriendsState.data[0].name+'</td><td>'+listOfFriendsState.data[0].location+'</td></tr>'
-    // console.log("listOfFriends:")
-    // console.log(listOfFriends)
     
-    //     console.log("listofFriends inside for loop:")
-    //         console.log(listOfFriends)
-    // }
-    // console.log("listofFriends outside for loop:")
-    //     console.log(listOfFriends)
-    listOfFriends += '<tr><td>FriendName1</td><td>FriendLocation1</td></tr>' //works fine
+    const getFriendsArrWAxios = async () => {
+        await axios.get('http://localhost:4000/users/getFriendsList', {headers: {'auth-token': token}})
+        .then(({data}) => {
+            setListOfFriendsState(data) //e.g. {{ _id: "60628a792b6f241e5d881394", name: "Emily", location: "Portland, Oregon" }}
+            console.log('data we get back from axios get request:')
+            console.log(data)
+        })
+        .catch(error => console.error(`Error: ${error}`))
+    }
+
+    console.log("listOfFriendsState:")    
+    console.log(listOfFriendsState)
+
+    if (listOfFriendsState.length > 0) {
+        for(let i = 0; i < listOfFriendsState.length; i++) {
+            // listOfFriends += '<tr><td>'+listOfFriendsState[i].name+'</td><td>'+listOfFriendsState[i].location+'</td></tr>'
+            listOfFriends += 
+                (`<tr><td>${listOfFriendsState[i].name}</td><td>${listOfFriendsState[i].location}</td><td><button type="button" onClick=${DeleteFriendFunc(listOfFriendsState[i].name)}>Delete Friend</button></td></tr>`)
+        }
+    } 
     return listOfFriends
+}
+
+const DeleteFriendFunc = (friendName) => {
+    console.log("token2:")
+    console.log(token)
+    axios.post('http://localhost:4000/users/deleteFriend', {
+        headers: {'auth-token': token}, 
+        'name': friendName
+        })
+        .then(console.log('user deleted'))
+        .catch(error => console.error(`Error: ${error}`))
 }
 
 
 export default function FriendsList() {
-    // const {friendsInfo, setFriendsInfo} = useFriendsInfoContext()
-    // console.log("friendsInfo:")
-    // console.log(friendsInfo)
-    // console.log("friendsInfo from local storage:")
-    // console.log(localStorage.getItem('friendsInfo'))
-    // console.log(typeof localStorage.getItem('friendsInfo'))
-    // console.log("-------------")
-    
+
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <th>Name</th>
-                    <th>Location</th>
-                </tr>
-                <tr>
-                    <td>Name1</td>
-                    <td>Paris, France</td>
-                </tr>
-                {parse(useAxiosToGetFriendsArr())} 
-            </tbody>
-        </table>
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th>Delete button</th>
+                    </tr>
+                    <tr>
+                        <td>Hardcode Name1</td>
+                        <td>Hardcode Location1</td>
+                    </tr>
+                    {parse(GiveFriendsArr())} 
+                </tbody>
+            </table>
+            {/* {parse(GiveFriendsArr())}  */}
+        </div>
     )
 }
