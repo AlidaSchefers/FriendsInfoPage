8c4f5d086985616b309088bb976db751e6ceeec9
AlidaSchefers <alidadschefers@gmail.com>
2021-03-30 20:31:10 -0400

    successfully displays weather description and pic based on location input

--- frontend/src/components1/GetaWeatherPic.js
+++ frontend/src/components1/GetaWeatherPic.js
@@ -1,23 +1,60 @@
-import React, {useState} from 'react'
-import Welcome from './Welcome1'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
-export default function GetaWeatherPic () {
-    const renderInputBox = () => {
+//import weather images
+import Thunderstorm from '../publicpics/drizzle1.jpg'
+import Drizzle from '../publicpics/drizzle1.jpg'
+import Clouds from '../publicpics/clouds1.jpg'
+import Snow from '../publicpics/snow1.jpg'
+import Rain from '../publicpics/rain1.jpg'
+import Clear from '../publicpics/clear1.jpg'
 
+export default function GivePic(locationInput) {
+    const [mainWeather, setMainWeather] = useState('')
+    useEffect(() => {
+        getMainWeather(locationInput)
+    }, [locationInput])
+
+    const getMainWeather = (locationInput) => { //gives me the current weather description (e.g. Rain") for the location (e.g. "Paris, France")
+        axios.post('http://localhost:4000/users/getFriendsInfo/weather', locationInput)
+        .then(res => {
+            setMainWeather(res.data.mainWeather) //e.g. "Clear", "Rain"
+        })
+        .catch(error => console.error(`Error: ${error}`))
     }
-    const renderButton = () => {
+    
+    if(mainWeather === '') {
         return(
-            <button 
-                // onClick={} //display the picture
-            >Display Weather Picture
-            </button>
+            <div>
+            <h3>Current Weather: </h3>
+            </div>
+        )
+    }else {
+        return(
+            <div>
+            <h3>Current Weather: {mainWeather}</h3>
+            <img src={giveImageVar(mainWeather)} alt="alt text" width="500" height="600"></img>
+            {/* <img src={Clear} alt="alt text" width="500" height="600"></img> */}
+            </div>
         )
     }
+}
 
-    const renderWeatherPic = () => {
-        return (
-            
-        )
+const giveImageVar = (mainWeather) => {
+    switch (mainWeather) {
+        case "Thunderstorm":
+            return Thunderstorm
+        case "Drizzle":
+            return Drizzle
+        case "Snow":
+            return Snow
+        case "Rain":
+            return Rain
+        case "Clear":
+            return Clear
+        case "Clouds":
+            return Clouds
+        default:
+            break;
     }
-
 }
