54373225173488a5457c5690fdd19c211481047c
AlidaSchefers <alidadschefers@gmail.com>
2021-04-01 15:37:04 -0400

    added explanatory comment

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -15,7 +15,7 @@ module.exports = function(req, res, next) {
     if(!token) return res.status(401).send('Access Denied')
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET_KEY)
-        req.user = verified //verified returns { _id: '6023e9d528f50c4f24f4e728', iat: 1612966385 }
+        req.user = verified //verified returns a JSON with _id and iat from token (e.g. { _id: '6023e9d528f50c4f24f4e728', iat: 1612966385 })
         next()
     } catch (error) {
         res.status(400).send('Invalid Token')
